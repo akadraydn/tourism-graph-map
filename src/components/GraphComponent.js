a6dab92b610d5c9ff69f13a95b78394af459f927
Ahmet Kadir Aydın <116378375+akadraydn@users.noreply.github.com>
2024-07-17 15:56:21 +0300

    Update GraphComponent.js

--- src/components/GraphComponent.js
+++ src/components/GraphComponent.js
@@ -3,31 +3,33 @@ import React from 'react';
 import Select from 'react-select';
 
 const GraphComponent = ({ selectedYear, setSelectedYear }) => {
+  
   const options = Array.from({ length: 2023 - 2000 + 1 }, (_, i) => ({
     value: (2000 + i).toString(),
     label: (2000 + i).toString()
   }));
 
+  
   const customStyles = {
     control: (base) => ({
       ...base,
-      minWidth: 120,
-      fontSize: 12,
+      minWidth: 120, 
+      fontSize: 12,  
     }),
     menu: (base) => ({
       ...base,
-      fontSize: 12,
+      fontSize: 12,  
     }),
   };
 
   return (
     <Select
-      styles={customStyles}
-      options={options}
-      defaultValue={options.find(option => option.value === selectedYear)}
-      onChange={selectedOption => setSelectedYear(selectedOption.value)}
+      styles={customStyles} 
+      options={options} 
+      defaultValue={options.find(option => option.value === selectedYear)} // Varsayılan değer olarak seçilen yılı kullan
+      onChange={selectedOption => setSelectedYear(selectedOption.value)} // Seçim yapıldığında yılı güncelle
     />
   );
 };
 
-export default GraphComponent;
+export default GraphComponent; 
