e280938f38ac7515405426f8e7d9b5ec4a9ba010
Ahmet Kadir Aydın <116378375+akadraydn@users.noreply.github.com>
2024-07-17 16:00:29 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-// src/App.js
 import React, { useState } from 'react';
 import DataLoader from './DataLoader';
-import MapComponent from './components/MapComponent';
+import GlobeMapComponent from './components/GlobeMapComponent';
+import FlatMapComponent from './components/FlatMapComponent';
 import GraphComponent from './components/GraphComponent';
 import PanelComponent from './components/PanelComponent';
 
@@ -9,13 +9,35 @@ const App = () => {
   const [countryData, setCountryData] = useState([]);
   const [groupData, setGroupData] = useState([]);
   const [selectedYear, setSelectedYear] = useState('2022');
+  const [isGlobeMode, setIsGlobeMode] = useState(false);
+
+  const toggleMode = () => {
+    setIsGlobeMode(!isGlobeMode);
+  };
 
   return (
     <div style={{ height: '100vh', width: '100vw', position: 'relative' }}>
-      <div style={{ position: 'absolute', top: 100, left: 10, zIndex: 1000, width: '200px' }}>
+      <div style={{ position: 'absolute', top: 80, left: 10, zIndex: 1000, width: '200px' }}>
         <GraphComponent selectedYear={selectedYear} setSelectedYear={setSelectedYear} />
       </div>
-      <MapComponent data={countryData} selectedYear={selectedYear} />
+      <button
+        onClick={toggleMode}
+        style={{
+          position: 'absolute',
+          top: 10,
+          right: 10,
+          padding: '10px 20px',
+          cursor: 'pointer',
+          zIndex: 1000
+        }}
+      >
+        {isGlobeMode ? 'Flat Map' : 'Globe Map'}
+      </button>
+      {isGlobeMode ? (
+        <GlobeMapComponent data={countryData} selectedYear={selectedYear} />
+      ) : (
+        <FlatMapComponent data={countryData} selectedYear={selectedYear} />
+      )}
       <PanelComponent groupData={groupData} selectedYear={selectedYear} />
       <DataLoader setCountryData={setCountryData} setGroupData={setGroupData} />
     </div>
